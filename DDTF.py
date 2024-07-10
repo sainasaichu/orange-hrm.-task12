87385a5bca50623d0cc1645ffb91f71a66922831
Sahina Begaum <149920611+sainasaichu@users.noreply.github.com>
2024-07-10 19:21:08 +0530

    Add files via upload

---
+++ DDTF.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.NAME, "username")
+        self.password_input = (By.NAME, "password")
+        self.login_button = (By.XPATH, "//button[@type='submit']")
+
+    def enter_username(self, username):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.username_input)
+        ).send_keys(username)
+
+    def enter_password(self, password):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.password_input)
+        ).send_keys(password)
+
+    def click_login_button(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.login_button)
+        ).click()
+
+
+
